4f679e6d8efca0e20dc4977d23f2c80580f716b0
Rupert Lloyd <rupert.lloyd@gmail.com>
2015-06-26 14:09:55 -0400

    simplified the tank demo

--- examples/kivy/tank/main.py
+++ examples/kivy/tank/main.py
@@ -9,6 +9,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.slider import Slider
 from kivy.animation import Animation
 
+from brickfinder import BrickFinderWidget
 try:
     from jnius import detach
 except:
@@ -33,11 +34,11 @@ class TankApp(App):
     def build(self):
         self.manager = ScreenManager(transition=SlideTransition(
             duration=.15))
-        self.init_log()
-        self._doc = RstDocument(text='')
 
+        bfw = BrickFinderWidget(title='Tank Demo', start=True)
+        bfw.bind(brick=self.brickfound_callback)
         search_screen = Screen(name='search')
-        search_screen.add_widget(self._doc)
+        search_screen.add_widget(bfw)
         self.manager.add_widget(search_screen)
 
         tank_screen = Screen(name='tank')
@@ -52,53 +53,14 @@ class TankApp(App):
         self.manager.current = 'search'
         return self.manager
 
-    def init_log(self):
-        self._logbuffer = ['Tank Demo',
-                           '=========',
-                           '::',
-                           '']
-
-    def log(self,message):
-        """log a message to the console and gui"""
-        print(message)
-        self._logbuffer.append(message)
-
-    def on_start(self):
-        self._brick = None
-        self._connect_thread = threading.Thread(target=self.find_brick)
-        self._connect_thread.start()
-        Clock.schedule_interval(self.update_displayed_text, 0.5)
-        Clock.schedule_interval(self.check_brick_connection, 1.5)
-
-    def check_brick_connection(self,dt):
-        """wait for find_brick thread to complete, then print info"""
-        self.log(' Searching for a brick...')
-        self._connect_thread.join(timeout=0.5)
-        if not self._connect_thread.isAlive():
-            Clock.unschedule(self.check_brick_connection)
-            if self._brick:
-                self.log(' brick found :-)')
-                info = self._brick.get_device_info()
-                self.log(' Name:{}'.format(info[0].strip('\x00')))
-                self.log(' Address:{}'.format(info[1]))
-                self.log(' Firmware:{}'.format(self._brick.get_firmware_version()))
-                self.log(' Battery:{}mV'.format(self._brick.get_battery_level()))
-                Clock.schedule_once(self.start_tank,1.0)
-            else:
-                self.log(' brick not found :-(')
-                self.log(' Check brick is powered on and paired.')
-
-    def find_brick(self):
-        try:
-            self._brick = nxt.find_one_brick(debug=True)
-        finally:
-            # kivy/android will crash here if we don't detach
-            print('detaching thread')
-            detach()
-
-    def update_displayed_text(self,dt):
-        self._doc.text = '\n'.join(self._logbuffer)
-        self._doc.scroll_y = 0  # scroll to bottom
+    def brickfound_callback(self, instance, brick):
+        if brick:
+            print('we found it :-)')
+            self._brick = brick
+            Clock.schedule_once(self.start_tank,1.0)
+        else:
+            print('no brick :-(')
+            self.c
 
     def start_tank(self,dt):
         self._motleft = nxt.motor.Motor(self._brick, nxt.motor.PORT_B)
